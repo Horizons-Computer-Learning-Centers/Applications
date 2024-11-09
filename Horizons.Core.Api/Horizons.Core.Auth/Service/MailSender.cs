3c17e1fbfe44291544fdfb46fc4ec2e004107ce6
Benjamin Chuunga <benjamin.chuunga@gmail.com>
2024-11-08 21:11:03 -0500

    Email Sender

---
+++ Horizons.Core.Api/Horizons.Core.Auth/Service/MailSender.cs
@@ -0,0 +1,13 @@
+﻿using Horizons.Core.Auth.Service.Interface;
+using Horizons.Core.MailService;
+
+namespace Horizons.Core.Auth.Service
+{
+    public class MailSender : IMailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            await EmailSender.SendEmailAsync(email, subject, message);
+        }
+    }
+}
