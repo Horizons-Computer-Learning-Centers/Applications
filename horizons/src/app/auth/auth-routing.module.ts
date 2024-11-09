0c3841b29fe1caaf341906bf70d57db502e451ad
Benjamin Chuunga <benjamin.chuunga@gmail.com>
2024-11-08 22:52:03 -0500

    Confirm component

--- horizons/src/app/auth/auth-routing.module.ts
+++ horizons/src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ConfirmEmailComponent } from './confirm-email/confirm-email.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'confirm-email/:userId/:token',
+    component: ConfirmEmailComponent,
   },
 ];
 
---
+++ horizons/src/app/auth/confirm-email/confirm-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'horizons-confirm-email',
+  templateUrl: './confirm-email.component.html',
+  styleUrls: ['./confirm-email.component.scss']
+})
+export class ConfirmEmailComponent {
+
+}
